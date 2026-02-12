ac697801b886e96cfeda6f371127d19b73dc1524
Deepak Kumar S <ladhi5661@gmail.com>
2026-02-12 07:55:15 +0530

    day4

---
+++ Day4/p41.py
@@ -0,0 +1,9 @@
+import math
+# print(math.pi)
+# print(round(math.sqrt(64)))
+a = math.pi
+print(a)
+print(round(math.ceil(a)))
+print(round(math.floor(a)))
+print(math.sqrt(a))
+
---
+++ Day4/p42.py
@@ -0,0 +1,8 @@
+#Casting String to a Number
+zipcode = "1010101"
+print(zipcode)
+zipval = int(zipecode)
+print(type(zipcode))
+zipval = int("NewYork")
+
+
